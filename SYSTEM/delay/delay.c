9c1ac560ce67014598a8fb6ba7932e32edde0d05
wangzhuojun <wangzhuojun@minrrayav.com>
2025-03-03 14:48:16 +0800

    创建开发分支，上传keil v6 编译无错误无警告的HAL工程模板

---
+++ SYSTEM/delay/delay.c
@@ -0,0 +1,110 @@
+#include "../SYSTEM/sys/sys.h"
+#include "../SYSTEM/delay/delay.h"
+
+
+static uint32_t g_fac_us = 0;       /* us延时倍乘数 */
+
+/* 如果SYS_SUPPORT_OS定义了,说明要支持OS了(不限于UCOS) */
+#if SYS_SUPPORT_OS
+#include "FreeRTOS.h"
+#include "task.h"
+extern void xPortSysTickHandler(void);
+/**
+ * @brief     systick中断服务函数,使用OS时用到
+ * @param     ticks : 延时的节拍数  
+ * @retval    无
+ */  
+void SysTick_Handler(void)
+{
+    HAL_IncTick();
+    /* OS 开始跑了,才执行正常的调度处理 */
+    if (xTaskGetSchedulerState() != taskSCHEDULER_NOT_STARTED)
+    {
+        xPortSysTickHandler();
+    }
+}
+#endif
+
+/**
+ * @brief     初始化延迟函数
+ * @param     sysclk: 系统时钟频率, 即CPU频率(HCLK),等于系统时钟主频，单位MHz
+ * @retval    无
+ */  
+void delay_init(uint16_t sysclk)
+{
+#if SYS_SUPPORT_OS                                      /* 如果需要支持OS */
+    uint32_t reload;
+#endif
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);/* SYSTICK使用外部时钟源,频率为HCLK */
+    g_fac_us = sysclk;                                  /* 不论是否使用OS,g_fac_us都需要使用 */
+#if SYS_SUPPORT_OS                                      /* 如果需要支持OS. */
+    reload = sysclk;                                    /* 每秒钟的计数次数 单位为M */
+    reload *= 1000000 / configTICK_RATE_HZ;             /* 根据delay_ostickspersec设定溢出时间,reload为24位
+                                                         * 寄存器,最大值:16777216 */
+    SysTick->CTRL |= 1 << 1;                            /* 开启SYSTICK中断 */
+    SysTick->LOAD = reload;                             /* 每1/delay_ostickspersec秒中断一次 */
+    SysTick->CTRL |= 1 << 0;                            /* 开启SYSTICK */
+#endif 
+}
+
+/**
+ * @brief     延时nus
+ * @note      无论是否使用OS, 都是用时钟摘取法来做us延时
+ * @param     nus: 要延时的us数
+ * @note      nus取值范围: 0 ~ (2^32 / fac_us) (fac_us一般等于系统主频, 自行套入计算)
+ * @retval    无
+ */
+void delay_us(uint32_t nus)
+{
+    uint32_t ticks;
+    uint32_t told, tnow, tcnt = 0;
+    uint32_t reload = SysTick->LOAD;        /* LOAD的值 */
+    ticks = nus * g_fac_us;                 /* 需要的节拍数 */
+    
+    told = SysTick->VAL;                    /* 刚进入时的计数器值 */
+    while (1)
+    {
+        tnow = SysTick->VAL;
+        if (tnow != told)
+        {
+            if (tnow < told)
+            {
+                tcnt += told - tnow;        /* 这里注意一下SYSTICK是一个递减的计数器就可以了 */
+            }
+            else
+            {
+                tcnt += reload - tnow + told;
+            }
+            told = tnow;
+            if (tcnt >= ticks) 
+            {
+                break;                      /* 时间超过/等于要延迟的时间,则退出 */
+            }
+        }
+    }
+}
+
+/**
+ * @brief     延时nms
+ * @param     nms: 要延时的ms数 (0< nms <= (2^32 / fac_us / 1000))(fac_us一般等于系统主频, 自行套入计算)
+ * @retval    无
+ */
+void delay_ms(uint16_t nms)
+{
+    uint32_t i;
+    for (i = 0; i < nms; i++)
+    {
+        delay_us(1000);
+    }
+}
+
+/**
+ * @brief       HAL库内部函数用到的延时
+ * @note        HAL库的延时默认用Systick，如果我们没有开Systick的中断会导致调用这个延时后无法退出
+ * @param       Delay : 要延时的毫秒数
+ * @retval      None
+ */
+void HAL_Delay(uint32_t Delay)
+{
+     delay_ms(Delay);
+}
