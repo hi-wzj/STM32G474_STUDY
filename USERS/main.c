9bc7070e3feac2c53c8826c39c314602799c3a45
wangzhuojun <wangzhuojun@minrrayav.com>
2025-03-04 17:32:19 +0800

    修改时钟为板子的8M,串口实验

--- USERS/main.c
+++ USERS/main.c
@@ -3,25 +3,46 @@
 #include "../SYSTEM/delay/delay.h"
 
 #include "../MIDDLEWARS/Drivers/LED/led.h"
-#include "../MIDDLEWARS/Drivers/KEY/key.h"
-#include "../MIDDLEWARS/Drivers/EXTI/exti.h"
 
-/* 通过外部中断，就不需要扫描按键GPIO的输入，由中断事件来触发
-    这样实时性又高，代码量又少 */
 int main(void)
 {
-    uint8_t key;
+    uint8_t len;
+    uint16_t times = 0;
     
 	HAL_Init();
 	sys_stm32_clock_init(85, 2, 2, 4, 8);
     delay_init(170);
     
     led_init();
-    key_init(); // 要比extix_init先初始化
-    extix_init();
+    usart_init(115200);
     
     while(1)
     {
-        delay_ms(1000);
+        if(g_usart_rx_sta & 0x8000)
+        {
+            len = g_usart_rx_sta & 0x3fff;
+            printf("send msg:\n");
+            
+            HAL_UART_Transmit(&g_uart1_handle, (uint8_t *)g_usart_rx_buf, len, 1000);
+            
+            while(__HAL_UART_GET_FLAG(&g_uart1_handle, UART_FLAG_TC) != SET);
+            
+            printf("\n");
+            g_usart_rx_sta = 0;
+        }
+        else
+        {
+            times++;
+            if(times % 5000 == 0)
+            {
+                printf("wzj is god\n");
+                printf("hello!!\n");
+            }
+            
+            if(times % 200 == 0) printf("input data, enter over\n");
+            if(times % 30 == 0) LED0_TOGGLE();
+            delay_ms(10);
+        }
+            
     }
 }
